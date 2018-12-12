e301ee99572f19cf1386e37dc1ae9d9aea907bac
Nikolay Ryabov <k0lll0s@yandex.ru>
2018-12-12 10:41:35 +0300

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('projectOne', ['ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
